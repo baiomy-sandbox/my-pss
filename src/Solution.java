dbbca9ee89b612d05533c9fedd34261578713793
fathy <fathyatefbaiomy99@gmail.com>
2023-03-02 23:43:33 +0200

    recursion

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,22 @@
 public class Solution {
-    void play(){
-        System.out.println("play insi");
+    void play() {
+        printNum(6840);
+    }
+
+    void printNum(int num) {
+        if (num == 0) {
+            return;
+        }
+        System.out.println("num = " + num % 10);
+        printNum(num / 10);
+    }
+    int getSum(int num){
+    if(num==0)return 0;
+        System.out.println(num);
+    return (num%10)+getSum(num/10);
+    }
+    int getFactorial(int num){
+        if(num<=0)return 1;
+        return num*getFactorial(num-1);
     }
 }
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -5,16 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class SolutionTest extends AbsTest {
     @Test
     void case1() {
-
+        solution.play();
     }
 
-//    @Test
-//    void case2() {
-//
-//    }
-//
-//    @Test
-//    void case3() {
-//
-//    }
+    @Test
+    void case2() {
+        int num=9874;
+        int ans=solution.getSum(num);
+        System.out.println("num = "+num +" and sum = "+ans);
+        assertEquals(28,solution.getSum(num));
+    }
+    @Test
+    void case3() {
+        System.out.println(solution.getFactorial(4));
+    }
 }
