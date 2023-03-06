005963244355071fbdf107f5047d487347cbb412
fathy <fathyatefbaiomy99@gmail.com>
2023-03-06 05:12:48 +0200

    21. Merge Two Sorted Lists

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,40 @@
-public class Solution {
-    void play(){
-        System.out.println("play insi");
+
+  class ListNode {
+     int val;
+     ListNode next;
+      ListNode() {}
+     ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+class Solution {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1.val > list2.val) {
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
+        }
+        ListNode head = list1;
+        ListNode hold = list2;
+        while (list1 != null&&hold!=null) {
+            if (list1.next!=null){
+                if (list1.next.val<= hold.val&&hold.val >= list1.val){
+                    list1=list1.next;
+                    continue;
+                }
+            }
+            if (hold.val >= list1.val) {
+                ListNode temp = list1.next;
+                list1.next = hold;
+                hold = temp;
+                list1 = list1.next;
+                continue;
+            }
+            list1=list1.next;
+        }
+        return head;
     }
 }
