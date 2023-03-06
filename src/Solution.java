634dc17ed8f6b406e237d01777fb2c2381009216
fathy <fathyatefbaiomy99@gmail.com>
2023-03-06 05:14:33 +0200

    88. Merge Sorted Array

--- src/Solution.java
+++ src/Solution.java
@@ -1,40 +1,19 @@
-
-  class ListNode {
-     int val;
-     ListNode next;
-      ListNode() {}
-     ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
-
 class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1;
+        int j=n-1;
+        int k=nums1.length-1;
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
-        if (list1.val > list2.val) {
-            ListNode temp = list1;
-            list1 = list2;
-            list2 = temp;
-        }
-        ListNode head = list1;
-        ListNode hold = list2;
-        while (list1 != null&&hold!=null) {
-            if (list1.next!=null){
-                if (list1.next.val<= hold.val&&hold.val >= list1.val){
-                    list1=list1.next;
-                    continue;
-                }
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                k--;
+                i--;
+            } else{
+                nums1[k] = nums2[j];
+                k--;
+                j--;
             }
-            if (hold.val >= list1.val) {
-                ListNode temp = list1.next;
-                list1.next = hold;
-                hold = temp;
-                list1 = list1.next;
-                continue;
-            }
-            list1=list1.next;
         }
-        return head;
     }
 }
