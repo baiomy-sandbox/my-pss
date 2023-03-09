dc449be4273bebbd5f6c5d84265a5e2373799c19
fathy <fathyatefbaiomy99@gmail.com>
2023-03-10 01:46:20 +0200

    169. Majority Element

--- src/Solution.java
+++ src/Solution.java
@@ -1,57 +1,23 @@
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 
-class ListNode {
-     int val;
-     ListNode next;
-     ListNode(int x) {
-         val = x;
-         next = null;
-     }
- }
-
-public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if (headA==headB)return headA;
-        int lengthA=1;
-        int lengthB=1;
-        ListNode tailA=headA;
-        ListNode tailB=headB;
-        while (tailA.next!=null){
-            lengthA++;
-            tailA=tailA.next;
-            if (tailA==tailB)return tailA;
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int maxCount=nums.length/2;
+        int compare=nums[0];
+        int major=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (compare==nums[i]){
+                major++;
+            }else {
+                compare=nums[i];
+                major=1;
+                continue;
+            }
+            if (major>maxCount){
+                return compare;
+            }
         }
-        while (tailB.next!=null){
-            lengthB++;
-            tailB=tailB.next;
-            if (tailA==tailB)return tailB;
-        }
-        if (tailB!=tailA)return null;
-        tailA=headA;
-        tailB=headB;
-        ListNode intersection=null;
-        if (lengthA>lengthB){
-           while (lengthA>lengthB){
-               tailA=tailA.next;
-               lengthA--;
-           }
-           while (tailA!=tailB){
-               tailA=tailA.next;
-               tailB=tailB.next;
-               intersection=tailA;
-           }
-           return intersection;
-       }
-        while (lengthB>lengthA){
-            tailB=tailB.next;
-            lengthB--;
-        }
-        while (tailA!=tailB){
-            tailA=tailA.next;
-            tailB=tailB.next;
-            intersection=tailA;
-        }
-        return intersection;
+        return major;
     }
 }
