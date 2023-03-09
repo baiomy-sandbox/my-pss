06e80e6655e6f05fbd3ae7e074c5599b17fa1138
fathy <fathyatefbaiomy99@gmail.com>
2023-03-09 07:53:55 +0200

    160. Intersection of Two Linked Lists

--- src/Solution.java
+++ src/Solution.java
@@ -11,13 +11,47 @@ class ListNode {
  }
 
 public class Solution {
-    public boolean hasCycle(ListNode head) {
-        ListNode fast=head,slow=head;
-        while (fast!=null&&fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
-            if (fast==slow)return true;
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA==headB)return headA;
+        int lengthA=1;
+        int lengthB=1;
+        ListNode tailA=headA;
+        ListNode tailB=headB;
+        while (tailA.next!=null){
+            lengthA++;
+            tailA=tailA.next;
+            if (tailA==tailB)return tailA;
         }
-        return false;
+        while (tailB.next!=null){
+            lengthB++;
+            tailB=tailB.next;
+            if (tailA==tailB)return tailB;
+        }
+        if (tailB!=tailA)return null;
+        tailA=headA;
+        tailB=headB;
+        ListNode intersection=null;
+        if (lengthA>lengthB){
+           while (lengthA>lengthB){
+               tailA=tailA.next;
+               lengthA--;
+           }
+           while (tailA!=tailB){
+               tailA=tailA.next;
+               tailB=tailB.next;
+               intersection=tailA;
+           }
+           return intersection;
+       }
+        while (lengthB>lengthA){
+            tailB=tailB.next;
+            lengthB--;
+        }
+        while (tailA!=tailB){
+            tailA=tailA.next;
+            tailB=tailB.next;
+            intersection=tailA;
+        }
+        return intersection;
     }
 }
