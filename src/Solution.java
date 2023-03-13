558b184d98f735fa7e5ca32bb5af4925d607a97a
fathy <fathyatefbaiomy99@gmail.com>
2023-03-14 00:33:33 +0200

    191. Number of 1 Bits

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,20 @@
-
- public class TreeNode {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode() {}
-     TreeNode(int val) { this.val = val; }
-     TreeNode(int val, TreeNode left, TreeNode right) {
-         this.val = val;
-         this.left = left;
-         this.right = right;
-     }
- }
-
 class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
-if (nums==null||nums.length==0)return null;
-return constructArray(nums,0,nums.length-1);
+    public int[] countBits(int n) {
+        int num=0;
+        int[] sums=new int[n+1];
+        for (int i = 0; i <=n; i++) {
+            System.out.println("n = "+n+" count = "+count(n)+"pos = "+(i));
+            sums[i]=count(num);
+            num++;
+        }
+        return sums;
     }
-    public TreeNode constructArray(int[] nums,int left,int right){
-        if (left>right)return null;
-        int mid=left+(right-left)/2;
-        return new TreeNode(nums[mid],constructArray(nums,left,mid-1),constructArray(nums,mid+1,right));
+    public int count(int n) {
+        int sum = 0;
+        while(n != 0){
+            sum++;
+            n &= (n-1);
+        }
+        return sum;
     }
 }
