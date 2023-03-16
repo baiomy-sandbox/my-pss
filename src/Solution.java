080bca735447db6f51403ca49f359b774d43e04d
fathy <fathyatefbaiomy99@gmail.com>
2023-03-16 09:42:24 +0200

    217. Contains Duplicate

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
 
-  class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- }
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
 
-  class Solution {
-      public ListNode reverseList(ListNode head) {
-          ListNode prev = null;
-          ListNode current = head;
-          while(current != null) {
-              ListNode next = current.next;
-              current.next = prev;
-              prev = current;
-              current = next;
-          }
-          return prev;
-      }
-  }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if(set.contains(num))return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
