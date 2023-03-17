b1f6784941dd7b0bb94cfae4dce7c064a4b10e61
fathy <fathyatefbaiomy99@gmail.com>
2023-03-17 03:05:55 +0200

    387. First Unique Character in a String

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,12 @@
-import java.util.Arrays;
-
 class Solution {
-    public int[] intersect(int[] nums1, int[] nums2) {
-        int[] allNums = new int[1001];
-        int[] small = nums1.length <= nums2.length ? nums1 : nums2;
-        int[] big = nums1.length > nums2.length ? nums1 : nums2;
-        for (int i = 0; i < small.length; i++) {
-            allNums[small[i]]++;
+    public int firstUniqChar(String s) {
+        int[] charSum=new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            charSum[s.charAt(i)-'a']++;
         }
-        int[] intersect = new int[small.length];
-        int maxLength = 0;
-        for (int i = 0; i < big.length; i++) {
-            if (allNums[big[i]] > 0) {
-                intersect[maxLength++] = big[i];
-                allNums[big[i]]--;
-            }
+        for (int i = 0; i < s.length(); i++) {
+            if (charSum[s.charAt(i)]==1)return i;
         }
-        return copyArray(intersect, maxLength);
-    }
-    int[] copyArray(int[] arr , int maxLength){
-        int[] newArr=new int[maxLength];
-        for (int i = 0; i < maxLength; i++) {
-            newArr[i]=arr[i];
-        }
-        return newArr;
+        return -1;
     }
 }
