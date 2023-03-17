29ee8885f8ebee563608c3d23f97ece31abb4cb4
fathy <fathyatefbaiomy99@gmail.com>
2023-03-17 02:55:50 +0200

    350. Intersection of Two Arrays II

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 class Solution {
-    public boolean isPowerOfThree(int n) {
-        return n>0&&1162261467%n==0;
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int[] allNums = new int[1001];
+        int[] small = nums1.length <= nums2.length ? nums1 : nums2;
+        int[] big = nums1.length > nums2.length ? nums1 : nums2;
+        for (int i = 0; i < small.length; i++) {
+            allNums[small[i]]++;
+        }
+        int[] intersect = new int[small.length];
+        int maxLength = 0;
+        for (int i = 0; i < big.length; i++) {
+            if (allNums[big[i]] > 0) {
+                intersect[maxLength++] = big[i];
+                allNums[big[i]]--;
+            }
+        }
+        return copyArray(intersect, maxLength);
+    }
+    int[] copyArray(int[] arr , int maxLength){
+        int[] newArr=new int[maxLength];
+        for (int i = 0; i < maxLength; i++) {
+            newArr[i]=arr[i];
+        }
+        return newArr;
     }
 }
