0f6a3fecdf5126fbef407437abe950b6f80382c7
fathy <fathyatefbaiomy99@gmail.com>
2023-03-18 06:23:25 +0200

    3. Longest Substring Without Repeating Characters

--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,17 @@
-    class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+import java.util.HashSet;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet();
+        int max = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            while(!set.add(s.charAt(right))) {
+                set.remove(s.charAt(left++));
+            }
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
 }
 
-    class Solution {
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            ListNode head=new ListNode(l1.val+l2.val);
-            ListNode addition=head;;
-            int lastSum=(l1.val+l2.val)/10;
-            l1=l1.next;
-            l2=l2.next;
-            System.out.println("l1 = "+l1.val+" l2 = "+l2.val+" last sum = "+lastSum+" s = "+((l1.val+l2.val)%10+lastSum)%10+" addition = "+addition.val+" next = "+addition.val);
-            while (l1!=null||l2!=null){
-                int l1v=l1==null?0:l1.val;
-                int l2v=l2==null?0:l2.val;
-                int s=((l1v+l2v)%10+lastSum)%10;
-                addition.next=new ListNode(s);
-                System.out.println("l1 = "+l1v+" l2 = "+l2v+" last sum = "+lastSum+" s = "+s+" addition = "+addition.val+" next = "+addition.val);
-                addition=addition.next;
-                lastSum=(l1v+l2v)/10;
-                if (l1!=null)l1=l1.next;
-                if (l2!=null)l2=l2.next;
-            }
-            return head;
-        }
-    }
