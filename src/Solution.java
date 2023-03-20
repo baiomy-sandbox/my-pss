2e37c40b61720faa34ccac2ac1120da078748242
fathy <fathyatefbaiomy99@gmail.com>
2023-03-20 03:41:02 +0200

    11. Container With Most Water

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,14 @@
 class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n){
-        ListNode ans=new ListNode();
-        ans.next=head;
-        ListNode fast=ans;
-        ListNode slow=ans;
-
-        for(int i=1;i<=n;i++){
-            fast=fast.next;
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length-1;
+        int max = 0;
+        while(l < r){
+            int minHeight = (height[l] < height[r] ? height[l] : height[r]);
+            int area = minHeight * (r - l);
+            if(area > max) max = area;
+            while(height[l] <= minHeight && l < r ) l++;
+            while(height[r] <= minHeight && l < r ) r--;
         }
-
-        while(fast.next!=null){
-            slow=slow.next;
-            fast=fast.next;
-        }
-        slow.next=slow.next.next;
-        return ans.next;
+        return max;
     }
 }
